4c3332f7552856cc8857147a4b4c979bc7a72563
Johny k Mathew <kmjohny@users.noreply.github.com>
2016-12-12 13:04:45 -0500

    Fix permission on nvim/install.sh. Fix checks before install.

--- eclim/install.sh
+++ eclim/install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 # Check for eclim
-if test ! $(which eclim) || test ! $(which eclimd) ; then 
-  echo "  Installing eclim for you."
-  ECLIM_DOWNLOAD_URL=https://github.com/ervandew/eclim/releases/download/2.6.0/eclim_2.6.0.jar
-  ECLIM_DOWNLOAD_FILE=/tmp/eclim.jar
-  ECLIPSE_HOME=/Applications/Eclipse.app/Contents/Eclipse
-  
+
+ECLIM_DOWNLOAD_URL=https://github.com/ervandew/eclim/releases/download/2.6.0/eclim_2.6.0.jar
+ECLIM_DOWNLOAD_FILE=/tmp/eclim.jar
+ECLIPSE_HOME=/Applications/Eclipse.app/Contents/Eclipse
+
+if [ -d $ECLIPSE_HOME ]; then 
+  echo "  Installing eclim for you."  
   curl -Lo $ECLIM_DOWNLOAD_FILE $ECLIM_DOWNLOAD_URL
 
   if [ -f $ECLIM_DOWNLOAD_FILE ]; then
-    java -Dvim.files=$HOME/.vim -Declipse.home=$ECLIPSE_HOME -jar $ECLIM_DOWNLOAD_FILE install
+    java -Dvim.files=$HOME/.vim -Declipse.home=$ECLIPSE_HOME -jar $ECLIM_DOWNLOAD_FILE uninstall
 
     rm $ECLIM_DOWNLOAD_FILE
     
-    ln -s $ECLIPSE_HOME/eclim /usr/local/bin/eclim
-    ln -s $ECLIPSE_HOME/eclimd /usr/local/bin/eclimd
+#    ln -s $ECLIPSE_HOME/eclim /usr/local/bin/eclim
+#    ln -s $ECLIPSE_HOME/eclimd /usr/local/bin/eclimd
  fi
 fi
