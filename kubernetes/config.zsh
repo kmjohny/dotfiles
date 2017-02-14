119e40c6c8316b8cee3f13c3b2da3e974085a033
Johny k Mathew <kmjohny@users.noreply.github.com>
2017-02-13 23:09:45 -0500

    - Add docker-machine-driver-xhyve to the brew install list.

---
+++ kubernetes/config.zsh
@@ -0,0 +1,6 @@
+# minikube requires docker-machine-driver-xhyve which needs root owner and uid.
+if test $(which docker-machine-driver-xhyve)
+then
+  sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+  sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+fi
