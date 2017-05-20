f74dc13f97eaf97060f782829113c1e4ab078029
Johny k Mathew <kmjohny@users.noreply.github.com>
2017-05-20 12:10:27 -0400

    - Add minikube aliases

---
+++ minikube/aliases.zsh
@@ -0,0 +1,5 @@
+# ------------------------------------
+# minikube alias
+# ------------------------------------
+
+alias mkss="minikube start --extra-config=apiserver.Features.EnableSwaggerUI=true"
