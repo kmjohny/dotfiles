d3620b368562a07647675b23b8010d49fda0279e
Johny Mathew <mathewj3@upmc.edu>
2024-04-05 09:18:56 -0400

    Move gp alias to a separate file

---
+++ myupmc/gp.zsh
@@ -0,0 +1,3 @@
+# Global Protect
+alias gp-up="launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangps.plist; launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangpa.plist"
+alias gp-down="launchctl unload /Library/LaunchAgents/com.paloaltonetworks.gp.pangps.plist; launchctl unload /Library/LaunchAgents/com.paloaltonetworks.gp.pangpa.plist"
